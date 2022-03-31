427765ecc272be93630fd4e1a408f531970dd6de
Edson Nascimento <edsnascimento89@gmail.com>
2022-03-31 12:24:54 -0300

    displaying user events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -83,7 +83,10 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', 
+            ['events' => $events, 'eventasparticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id)
@@ -96,8 +99,13 @@ class EventController extends Controller
 
     public function edit($id)
     {
+        $user = auth()->user();
 
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
